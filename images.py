6bc9e1b3eb162a8e1169d33d3fe81622c1744658
yanoshick <57157843+yanoshick@users.noreply.github.com>
2022-01-09 20:55:22 +0100

    destroying image windows and some waitings between

--- images.py
+++ images.py
@@ -1,18 +1,22 @@
 # opencv imagaes application for python3
 import cv2
 import numpy as np
-
+import time
 
 
 print("Hello opencv images!!!")
 
 img = cv2.imread('lena.png')
 cv2.imshow("Lena original", img)
+cv2.waitKey(3000)
+cv2.destroyAllWindows()
+
 
 affine_warp = np.array([[2, 0, 0], [0, 1, 0]], dtype=np.float32)
 dsize = (img.shape[1]*2, img.shape[0])
 warped_img = cv2.warpAffine(img, affine_warp, dsize)
 
 cv2.imshow("2x Horizontal Stretching", warped_img)
-cv2.waitKey(0)
+cv2.waitKey(8000)
+cv2.destroyAllWindows()
 
